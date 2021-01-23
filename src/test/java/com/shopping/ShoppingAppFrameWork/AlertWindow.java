f505e3c524336ed1997377e3477c1e277b857ca4
SS3900 <sivarammrn@gmail.com>
2021-01-23 12:36:55 +0530

    New class-AlertWindow added

---
+++ src/test/java/com/shopping/ShoppingAppFrameWork/AlertWindow.java
@@ -0,0 +1,10 @@
+package com.shopping.ShoppingAppFrameWork;
+
+public class AlertWindow {
+
+	
+	public static void main(String[]args)
+	{
+		System.out.println("This class is created to AlertWindow");
+	}
+}
