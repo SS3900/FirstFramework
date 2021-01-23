6376b6a3c7f3f466a1c42a436f0943329382e69f
SS3900 <sivarammrn@gmail.com>
2021-01-23 12:57:01 +0530

    Code added for alert

--- src/test/java/com/shopping/ShoppingAppFrameWork/AlertWindow.java
+++ src/test/java/com/shopping/ShoppingAppFrameWork/AlertWindow.java
@@ -1,11 +1,26 @@
 package com.shopping.ShoppingAppFrameWork;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class AlertWindow {
 
 	
 	public static void main(String[]args)
 	{
-		// Create the Alert window
-		System.out.println("This class is created to AlertWindow");
+		System.setProperty("webdriver.chrome.driver", "./drivers/geckodriver.exe");
+		WebDriver fDriver = new FirefoxDriver();
+		fDriver.get("https://demoqa.com/");
+		fDriver.manage().window().maximize();
+		
+		fDriver.findElement(By.xpath("//div[@class='category-cards']/div[3]")).click();
+		fDriver.findElement(By.xpath("//div[@class='element-list collapse show']//li[@id='item-1']")).click();
+		fDriver.findElement(By.id("alertButton")).click();
+		
+		Alert a = fDriver.switchTo().alert();
+		a.accept();
+		
 	}
 }
