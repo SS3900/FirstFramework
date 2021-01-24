0d17def1fb113abc5ed673d5a76481d0a9038f9d
SS3900 <sivarammrn@gmail.com>
2021-01-24 20:01:03 +0530

    3rd Change

--- src/test/java/com/shopping/ShoppingAppFrameWork/AlertWindow.java
+++ src/test/java/com/shopping/ShoppingAppFrameWork/AlertWindow.java
@@ -1,5 +1,10 @@
 package com.shopping.ShoppingAppFrameWork;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -13,6 +18,10 @@ import org.testng.annotations.Test;
 
 public class AlertWindow {
 
+	static WebDriver fDriver;
+	static WebElement e;
+	
+	
 	public static WebElement scroll(WebElement element, WebDriver driver)
 	  {
 		  JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -24,15 +33,32 @@ public class AlertWindow {
 	public void newPage()
 	{
 		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
-		WebDriver fDriver = new FirefoxDriver();
-		fDriver.get(System.getProperty("url"));
+		fDriver = new FirefoxDriver();
+		fDriver.get("https://demoqa.com");
 		fDriver.manage().window().maximize();
 		
 		Wait wait = new WebDriverWait(fDriver,50);
 		
+		//********************************Window Handle*********************************//
 		fDriver.findElement(By.xpath("//div[@class='category-cards']/div[3]")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[1]")));
+		//e= fDriver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[1]"));
+		//scroll(e,fDriver);
+		fDriver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[1]")).click();
+		String parentWindow = fDriver.getWindowHandle();
+		System.out.println(parentWindow);
+		fDriver.findElement(By.id("tabButton")).click();
+		String childWindow = fDriver.getWindowHandle();
+		
+		if(!parentWindow.equals(childWindow)) {
+			fDriver.switchTo().window(childWindow).close();
+		}
+		
+		
+		//*******************************Window Alert************************************//
+		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[2]")));
-		WebElement e= fDriver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[2]"));
+		e= fDriver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[2]"));
 		scroll(e,fDriver);
 		fDriver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[2]")).click();
 		fDriver.findElement(By.id("alertButton")).click();
@@ -40,5 +66,20 @@ public class AlertWindow {
 		Alert a = fDriver.switchTo().alert();
 		a.accept();
 		
+		fDriver.findElement(By.id("timerAlertButton")).click();
+		fDriver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+		a.accept();
+		
+		fDriver.findElement(By.id("confirmButton")).click();
+		a.dismiss();
+		
+		fDriver.findElement(By.id("promtButton")).click();
+		a.sendKeys("Alert Confirmed");
+		a.accept();
+		
+		fDriver.close();
+		
 	}
+	
+	
 }
