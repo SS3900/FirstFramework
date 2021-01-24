6c3d81dff27219491bd0ca53cb0b223dc312dcbf
SS3900 <sivarammrn@gmail.com>
2021-01-24 17:33:53 +0530

    second commit

--- src/test/java/com/shopping/ShoppingAppFrameWork/AlertWindow.java
+++ src/test/java/com/shopping/ShoppingAppFrameWork/AlertWindow.java
@@ -1,12 +1,26 @@
 package com.shopping.ShoppingAppFrameWork;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.touch.ScrollAction;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AlertWindow {
 
+	public static WebElement scroll(WebElement element, WebDriver driver)
+	  {
+		  JavascriptExecutor js = (JavascriptExecutor) driver;
+		  js.executeScript("arguments[0].scrollIntoView();", element);
+		  return element;
+	  }
 	
 	public static void main(String[]args)
 	{
@@ -15,8 +29,13 @@ public class AlertWindow {
 		fDriver.get("https://demoqa.com/");
 		fDriver.manage().window().maximize();
 		
+		Wait wait = new WebDriverWait(fDriver,50);
+		
 		fDriver.findElement(By.xpath("//div[@class='category-cards']/div[3]")).click();
-		fDriver.findElement(By.xpath("//div[@class='element-list collapse show']//li[@id='item-1']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[2]")));
+		WebElement e= fDriver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[2]"));
+		scroll(e,fDriver);
+		fDriver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[2]")).click();
 		fDriver.findElement(By.id("alertButton")).click();
 		
 		Alert a = fDriver.switchTo().alert();
